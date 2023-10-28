3ba2781a295917541eefa19fa9813b8ddcdd4963
Mahesh K <maheshkuntumalla2001@gmail.com>
2023-10-28 19:54:07 +0530

    complete project commit

---
+++ backend/models/todo.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const todoSchema = new mongoose.Schema({
+    task : String,
+    done : {
+        type : Boolean,
+        default: false
+    }
+
+})
+
+const todoModel = mongoose.model("todos", todoSchema)
+
+module.exports = todoModel
